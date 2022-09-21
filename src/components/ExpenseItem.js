b8b29abb67c9b9316af14b86b9edcf3cd6c911b4
bisrat <bisrat.amtataw@gmail.com>
2022-09-21 11:09:57 -0400

    making date format readable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,10 +4,18 @@ function ExpenseItem(props){
     // const expenseDate = new Date(2022, 3, 28);
     // const expenseTitle = 'Car Insurance'
     // const expenseAmount = 294.67
+
+    const month = props.date.toLocalString('en-US', {month:'long'});
+    const day = props.date.toLocalString('en-US', {day:'2-digit'});
+    const year = props.date.getFullYear();
+
     return(
     <div className='expense-item'>
         <div >
-            {props.date.toDateString()}
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+            
             {/* {expenseDate.toDateString()} */}
         </div>
         <div className='expense-item__description'>
