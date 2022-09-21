c18c0fd164ed7b66571a76a090b96241f8f7e8db
bisrat <bisrat.amtataw@gmail.com>
2022-09-20 22:20:48 -0400

    expenseItem ccomponent update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(){
+    const expenseDate = new Date(2022, 3, 28);
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.67
     return(
     <div className='expense-item'>
         <div >
-            Date
+            {expenseDate.toDateString()}
         </div>
         <div className='expense-item__description'>
-            <h2>Title</h2>
+            <h2>{expenseTitle}</h2>
         <div className='expense-item__price'>
-            Amount
+           ${expenseAmount}
         </div>
         </div>
     </div>
