de9811bc782f1b5032e73c6ad56b2df9883cd3bb
bisrat <bisrat.amtataw@gmail.com>
2022-09-20 22:49:04 -0400

    expenseItem component update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import ExpenseItem from './components/ExpenseItem'
 function App() {
+  const expense =[
+    {
+      id:'e1', title:'Toilet Paper', amount:24.67, date:new Date(2022,03,12)
+    },
+    {id:'e2', title:'Car Insurance', amount:294.67, date:new Date(2022,04,14)}, 
+    {id:'e3', title:'TV', amount:664.67, date:new Date(2022,03,1)},
+    {id:'e4', title:'Monitor', amount:200, date:new Date(2022,02,12)}
+  ]
   return (
     <div className="App">
       <header className="App-header">
           * Learn React  
       </header> 
-      <ExpenseItem/>
+      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
+      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
+      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
+      <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,22 @@
 import './ExpenseItem.css'
 
-function ExpenseItem(){
-    const expenseDate = new Date(2022, 3, 28);
-    const expenseTitle = 'Car Insurance'
-    const expenseAmount = 294.67
+function ExpenseItem(props){
+    // const expenseDate = new Date(2022, 3, 28);
+    // const expenseTitle = 'Car Insurance'
+    // const expenseAmount = 294.67
     return(
     <div className='expense-item'>
         <div >
-            {expenseDate.toDateString()}
+            {props.date.toDateString()}
+            {/* {expenseDate.toDateString()} */}
         </div>
         <div className='expense-item__description'>
-            <h2>{expenseTitle}</h2>
+            <h2>{props.title}</h2>
+            {/* <h2>{expenseTitle}</h2> */}
         <div className='expense-item__price'>
-           ${expenseAmount}
+           {/* ${expenseAmount} */}
         </div>
+             ${props.amount}
         </div>
     </div>
     )
