2ad46be7d058c29a642218a02d1d5e35eafa78cf
bisrat <bisrat.amtataw@gmail.com>
2022-09-23 14:36:43 -0400

    expenseform component creating

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expenses = [
     {
-      id:'e1',
-       title:'Toilet Paper', 
-       amount:24.67, 
-       date:new Date(2022, 3, 12),
+      id: 'e1',
+       title: 'Toilet Paper', 
+       amount: 24.67, 
+       date: new Date(2022, 3, 12),
     },
     {
-      id:'e2', 
-      title:'Car Insurance', 
-      amount:294.67, 
-      date:new Date(2022, 4, 14),
+      id: 'e2', 
+      title: 'Car Insurance', 
+      amount: 294.67, 
+      date: new Date(2022, 4, 14),
     }, 
     {
-    id:'e3', 
-    title:'TV', 
-    amount:664, 
-    date:new Date(2022, 3,1),
+    id: 'e3', 
+    title: 'TV', 
+    amount: 664, 
+    date: new Date(2022, 3,1),
     },
     {
       id:'e4', 
       title:'Monitor', 
       amount:200, 
-      date:new Date(2022, 2, 12),
+      date: new Date(2022, 2, 12),
     },
   ]
 
