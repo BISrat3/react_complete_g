5d026d55d8843bd48ee47fc0a0a77ea503a9784c
bisrat <bisrat.amtataw@gmail.com>
2022-09-25 13:40:14 -0400

    filter method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
+import {useState} from "react"
 import './Expenses.css'
 import Card from "../UI/Card";
+import ExpensesFilter from "../NewExpense/ExpenseFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020')
+  const filterChangeHandler = selectedYear =>{
+    console.log ("Expenses.js")
+    console.log(selectedYear)
+    setFilteredYear(selectedYear)
+  }
   return (
+    <div>
     <Card className="expenses">
+      <ExpensesFilter selected ={filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,6 +36,7 @@ function Expenses(props) {
         date={props.items[3].date}
       />
     </Card>
+    </div>
   );
 }
 export default Expenses;
