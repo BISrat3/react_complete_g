b964d89d09a292203f4eb91bd9eb90306788aee8
bisrat <bisrat.amtataw@gmail.com>
2022-10-06 21:30:38 -0400

    button update

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,7 +5,7 @@ import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
     const [isEditing, setIsEditng] = useState(false);
     
-    const saveExpenseDataHandler = (enteredExpenseData)=>{
+    const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString(),
